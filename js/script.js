1bf6b56c16e5d1fa85e1ef586e280ea416a37f0e
Christine Tran <cristinetran97@gmail.com>
2017-11-05 00:22:51 -0700

    Updated the map key section, and added tab functions (script.js, map.html, map.css

--- js/script.js
+++ js/script.js
@@ -49,3 +49,27 @@ function openIconTab(iconName){
 
   document.getElementById(iconName).style.display = "block";
 }
+
+/*Key Tabs Functionality
+---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+function openKey(evt, keyName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(keyName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
