54b905295067cec39bdad2af672071ab0ca04c80
Christine Tran <cristinetran97@gmail.com>
2017-12-08 09:51:03 -0800

    updated design

--- js/script.js
+++ js/script.js
@@ -364,7 +364,7 @@ function openIconTab(evt, iconName){
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "30%";
+    document.getElementById("mySidenav").style.width = "300px";
 }
 
 /* Set the width of the side navigation to 0 */
